7f7606e1e6b1cb19ac3c10beb1afcb19105b02a0
Joseph Martin <josephmartin721@yahoo.com>
2021-02-24 05:14:18 -0600

    ^X X ""

--- script.js
+++ script.js
@@ -1,22 +1,20 @@
 // need buttons that progress after answering
 // need personal information submit box
 // need event listeners and score counter
+// how to store score records..
+// make timer do more than just count down
 
 function buildQuiz(){
-    // variable to store the HTML output
+
     const output = [];
   
-    // for each question...
     myQuestions.forEach(
       (currentQuestion, questionNumber) => {
   
-        // variable to store the list of possible answers
         const answers = [];
   
-        // and for each available answer...
         for(letter in currentQuestion.answers){
   
-          // ...add an HTML radio button
           answers.push(
             `<label>
               <input type='radio' name='question${questionNumber}' value='${letter}'>
@@ -25,16 +23,13 @@ function buildQuiz(){
             </label>`
           );
         }
-  
-        // add this question and its answers to the output
+
         output.push(
           `<div class='question'> ${currentQuestion.question} </div>
           <div class='answers'> ${answers.join('')} </div>`
         );
       }
     );
-  
-    // finally combine our output list into one string of HTML and put it on the page
     quizContainer.innerHTML = output.join('');
   }
 
